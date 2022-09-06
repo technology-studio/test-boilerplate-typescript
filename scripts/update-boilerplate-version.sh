113f615419275e224b4a54249f97b7eafda6ab07
Erik Slovak <erik.slovak@technologystudio.sk>
2022-09-06 11:44:58 +0200

    update script for boilerplate version

--- scripts/update-boilerplate-version.sh
+++ scripts/update-boilerplate-version.sh
@@ -2,7 +2,7 @@
 
 LATEST=$(
   git ls-remote -h git@bitbucket.org:technology-studio/test-boilerplate-private-typescript.git |
-  pcregrep -o1 "([\w]*)\trefs\/heads\/main"
+  grep refs/heads/main | awk '{ print $1 }'
 )
 echo $LATEST > .boilerplate-version
 echo "Updated boilerplate version to: $LATEST"
